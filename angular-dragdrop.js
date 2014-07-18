c8df6150f0674710ecc23fc1ff863a59e0d506fe
Raphael Amorim <rapha850@gmail.com>
2014-07-17 23:45:18 -0300

    make initial angular strucuture :)

--- angular-dragdrop.js
+++ angular-dragdrop.js
@@ -1,1 +1,44 @@
-// Okay, so where's code?
+'use strict';
+
+var app = angular.module("dragAndDrop", [])
+
+.directive('dropBox', function() {
+	return {
+		link: function(scope, el) {
+			el.attr('ondrop', 'drop(event)');
+			el.attr('ondragover', 'allowDrop(event)');
+		}
+	}
+})
+
+.directive('dropItem', function() {
+	return {
+		link: function(scope, el) {
+			el.attr('ondragstart', 'drag(event)');
+			el.attr('draggable', 'true');
+		}
+	}
+});
+
+// The controller
+
+function dragAndDropController($scope) {
+	$scope.items = ['one', 'two', 'three', 'four', 'five', 'six'];
+	$scope.dropped = [];
+}
+
+// Independent functions
+
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+
+function drag(ev) {
+	ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("Text");
+	ev.target.appendChild(document.getElementById(data));
+}
