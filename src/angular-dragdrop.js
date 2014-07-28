f719a841ccd42080e477c63ddd5ec3129a738956
Raphael Amorim <rapha850@gmail.com>
2014-07-28 18:09:51 -0300

    add comments and use droppable list using attr droppedItems to display dropped items

--- src/angular-dragdrop.js
+++ src/angular-dragdrop.js
@@ -2,16 +2,24 @@
 
 var app = angular.module("dragAndDrop", [])
 
+// Application Directives
+
 .directive('droppable', function() {
     return function(scope, element) {
         var el = element[0];
 
+        // Add element to droppable list
+        scope.droppable.push(el);
+
+        // Apply prototype to list dropped items
+        el.droppedItems = [];
+
+        // Event to drop action on droppable element
         el.addEventListener('drop', function(e) {
                 e.preventDefault();
                 var data = e.dataTransfer.getData("Text");
                 var element = document.getElementById(data);
                 e.target.appendChild(element);
-                scope.dropList.push(element);
 
                 var index = scope.dragList.indexOf(element);
                 scope.dragList.splice(index, 1);
@@ -21,6 +29,7 @@ var app = angular.module("dragAndDrop", [])
             false
         );
 
+        // Event to drag over action on droppable element
         el.addEventListener('dragover', function(e) {
                 e.preventDefault();
             },
@@ -33,30 +42,47 @@ var app = angular.module("dragAndDrop", [])
     return function(scope, element) {
         var el = element[0];
 
+        // Apply draggable property on element
         el.draggable = true;
 
+        // If element don't have any id, create random id
         if (!el.id) el.id = Math.floor((Math.random() * 100000) + 1);
 
+        // Event on drag start
         el.addEventListener('dragstart', function(e) {
                 e.dataTransfer.setData("Text", e.target.id);
+
+                // When element is on drag, add drag class
                 this.classList.add('drag');
             },
             false
         );
 
+        // Event on drag end
         el.addEventListener('dragend', function(e) {
+
+                // When drag over, remove drag class
                 this.classList.remove('drag');
             },
             false
         );
 
+        // Add this element on dragList
         scope.dragList.push(el);
     }
 });
 
-// The controller
+// Application Controller
 
 function dragAndDropController($scope) {
+    /*
+        Element list with allow drag
+    */
     $scope.dragList = [];
-    $scope.dropList = [];
+
+    /*
+        Element list with allow drop, to see dropped items on the element.
+        Use droppedItems property on each element.
+    */
+    $scope.droppable = [];
 }
