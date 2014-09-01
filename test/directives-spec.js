eaf77c124a21315a360c8378c68c27d1afb85b57
Rodrigo Pinto <rodrigopqn@gmail.com>
2014-08-31 21:16:22 -0300

    Add an spec example.

---
+++ test/directives-spec.js
@@ -0,0 +1,22 @@
+"use strict";
+
+describe("Directive: droppable", function () {
+   beforeEach(function(){
+      angular.module("dragAndDrop")
+   });
+
+  	it("something goes here", function () {
+  	});
+
+});
+
+describe("Directive: draggable", function () {
+   beforeEach(function () {
+      angular.module("dragAndDrop")
+   });
+
+
+	it("something goes here", function () {
+
+	});
+});
